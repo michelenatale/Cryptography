73dd63b1e7f8770dfe4ce36ec2346cfad4c6546b
michelenatale <104991886+michelenatale@users.noreply.github.com>
2024-09-28 15:50:58 +0200

    Update

---
+++ Signatures/MyEcSchnorrConception/EasySignature/Signatures/MultiSignature/MultiSignInfo.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+
+
+namespace michele.natale.Signatures;
+
+using SignatureServices;
+
+public class MultiSignInfo
+{
+  public Guid ID { get; set; } = Guid.Empty;
+  public string Name { get; set; } = string.Empty;
+  public string Sign { get; set; } = string.Empty;
+  public string Message { get; set; } = string.Empty;
+  public string PublicKey { get; set; } = string.Empty;
+
+  public MultiSignInfo()
+  {
+  }
+
+  public MultiSignInfo(
+    string name, string msg,
+    byte[] pupkey, byte[] sign)
+  {
+    this.Message = msg;
+    this.ID = Guid.NewGuid();
+    this.Sign = Convert.ToHexString(sign);
+    this.PublicKey = Convert.ToHexString(pupkey);
+    this.Name = name ?? SignaturesServices.ToRngName();
+  }
+
+
+}
---
+++ Signatures/MyEcSchnorrConception/EasySignature/Signatures/MultiSignature/SignVerifyInfo.cs
@@ -0,0 +1,54 @@
+﻿
+
+
+using System.Security.Cryptography;
+
+namespace michele.natale.Signatures;
+
+public class SignVerifyInfo
+{
+
+  public byte[] MultiSign { get; set; } = [];
+  public bool MultiVerify { get; set; } = false;
+  public MultiSignInfo[] MultiSignInfos { get; set; } = [];
+
+  public SignVerifyInfo(MultiSignInfo[] multi_sign_infos)
+  {
+    var msg = multi_sign_infos.First().Message;
+    var istrue = multi_sign_infos.All(x => x.Message.SequenceEqual(msg));
+    if (istrue)
+    {
+      this.MultiSignInfos = multi_sign_infos;
+      this.MultiSign = Multi_Sign(multi_sign_infos);
+      this.MultiVerify = Multi_Verifiy(multi_sign_infos, this.MultiSign);
+    }
+  }
+
+  public static byte[] Multi_Sign(MultiSignInfo[] multi_sign_infos)
+  {
+    var msg = multi_sign_infos.First().Message;
+    var istrue = multi_sign_infos.All(x => x.Message.SequenceEqual(msg));
+    if (!istrue) return [];
+
+    var sgns = multi_sign_infos.OrderBy(x => x.Sign).ToArray();
+    var signs = sgns
+      .Select(s => Convert.FromHexString(s.Sign)).ToArray();
+
+    var max_length = signs.Max(x => x.Length);
+    var result = new byte[max_length];
+    foreach (var sn in multi_sign_infos)
+    {
+      var sgn = Convert.FromHexString(sn.Sign);
+      for (var i = 0; i < max_length; i++)
+        result[i % result.Length] ^= sgn[i % sgn.Length];
+    }
+    var half_length = signs.Length / 2;
+    return SHA512.HashData(result.Take(half_length).ToArray())
+      .Concat(SHA512.HashData(result.Skip(half_length).ToArray())).ToArray();
+  }
+
+
+  public static bool Multi_Verifiy(MultiSignInfo[] multi_sign_infos, byte[] multi_signatures) =>
+   multi_signatures.SequenceEqual(Multi_Sign(multi_sign_infos));
+
+}
